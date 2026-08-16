6032e946c5b30b306494b62af7bc1c0a177221d3
charanjtej <charantejj@gmail.com>
2026-08-17 00:13:06 +0530

    Explain Statement

--- 001_HelloWorld/HelloWorld.java
+++ 001_HelloWorld/HelloWorld.java
@@ -2,7 +2,15 @@
 // we can use JShell (Java Shell) is the tool, we'll be using to achieve this,
 class HelloWorld{
 	public static void main(String[] args){
+		//The Following line is known as `statement`
 		System.out.println("Hello World");
+		//What is `statement`?
+		/*
+			A complete command to be executed.
+			It can include one or more `expressions`.
+			HERE: Command to display some text on console screen using Java Syntax.
+			What to be diplayed is specified in quotes "".
+		*/
 	}
 }
 
