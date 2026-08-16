18b2508e39e60bb0a432864ae78bc582fc1b9d7e
charanjtej <charantejj@gmail.com>
2026-08-17 00:04:45 +0530

    First Script to Print Text onto console

---
+++ 001_HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+// To print a text on to screen
+// we can use JShell (Java Shell) is the tool, we'll be using to achieve this,
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
